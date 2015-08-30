b3c97bad1bbb4aa4ae6f0b2103a4c7cae55a1dbf
Dong Yuan <yuandong@unitedstack.com>
2015-08-30 15:55:08 +0800

    Finish Multiply_Strings
    Signed-off-by: Dong Yuan <yuandong@unitedstack.com>

--- LeetCode/006.Multiply_Strings.cpp
+++ LeetCode/006.Multiply_Strings.cpp
@@ -13,20 +13,42 @@ public:
         
         int index = 0;
         for (index = 0; index < s.size(); index++) {
-            l[s.size() - index] = s[index] - '0';
+            l[s.size() - index - 1] = s[index] - '0';
         }
         return l;
     }
     string vector_to_string(vector<int> a) {
-        string s(a.size(), '0');
-        int index = 0;
-        for (index = 0; index < s.size(); index++) {
-            s[index] = a[index] + '0';
+        int size = a.size();
+        while (a[size - 1] == 0) {
+            size--;
+        }
+        
+        string s(size, '0');
+        for (int i = 0; i < size; i++) {
+            s[s.size() - i -1] = a[i] + '0';
         }
         return s;
     }
     
+    void _multiply(vector<int> a, vector<int> b, vector<int>* sum) {
+        for (int i = 0; i < a.size(); i++) {
+            for (int j = 0; j < b.size(); j++) {
+                (*sum)[i + j] += a[i] * b[j];
+            }
+        }
+        
+        for (int i = 0; i < sum->size() - 1; i++) {
+            (*sum)[i + 1] += (*sum)[i] / 10;
+            (*sum)[i] %= 10;
+        }
+    }
+    
     string multiply(string num1, string num2) {
+        
+        if (num1 == "0" || num2 == "0") {
+            return "0";
+        }
+        
         vector<int> vector_a(num1.size());
         vector<int> vector_b(num2.size());
         vector<int> vector_sum(num1.size() + num2.size());
@@ -34,8 +56,7 @@ public:
         vector_a = string_to_vector(num1);
         vector_b = string_to_vector(num2);
         
-        //_multiply(vector_a, vector_b, vector_sum);
-        
+        _multiply(vector_a, vector_b, &vector_sum);
         
         string result;
         result = vector_to_string(vector_sum);
@@ -46,8 +67,8 @@ public:
 
 int main(int argc, char* argv[]) {
     Solution006 solution;
-    string a("1234567890");
-    string b("1234567890");
+    string a("0");
+    string b("0");
     
     cout << solution.multiply(a, b) << endl;
     return 0;
